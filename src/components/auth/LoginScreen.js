f9e1222837eab7047fce600822a5b7e76664a01b
Kevin Paima M <kpaimam@uni.pe>
2021-11-10 23:50:13 -0500

    añadido detalles

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -5,9 +5,11 @@ export const LoginScreen = () => {
             <div className="container login-container">
                 <div className="row">
                     <div className="col-md-6 login-form-1">
-                        <h3>Ingreso</h3>
+                        <h3>Bienvenido</h3>
                         <form>
+
                             <div className="form-group">
+                                <p>Email:</p>
                                 <input 
                                     type="text"
                                     className="form-control"
@@ -15,11 +17,14 @@ export const LoginScreen = () => {
                                 />
                             </div>
                             <div className="form-group">
+                                <p>Contraseña:</p>
                                 <input
                                     type="password"
                                     className="form-control"
                                     placeholder="Ingresa tu contraseña:"
                                 />
+                                
+                                <p><a href='./forgotpassword'><>¿Olvidaste la contraseña?</></a></p>
                             </div>
                             <div className="form-group">
                                 <input 
@@ -32,6 +37,7 @@ export const LoginScreen = () => {
                     </div>
     
                     <div className="col-md-6 login-form-2">
+                        ¿Todavía no tienes una cuenta? <a href='./singup' ><b>Cree una ahora</b></a>
                     </div>
 
                 </div>
